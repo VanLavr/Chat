4dde24ea80861f54d7c7c6b76f15a8d1fd336c48
VanLavr <lavr.i-n@mail.ru>
2023-10-12 23:05:46 +0300

    user delivery seems to be ready

--- internal/user/delivery/user_handler.go
+++ internal/user/delivery/user_handler.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"chat/models"
+	jwtmiddleware "chat/pkg/jwt_middleware"
 	"errors"
 	"strconv"
 
@@ -10,13 +11,19 @@ import (
 
 type UserHandler struct {
 	usecase models.UserUsecase
+	jwtmiddleware.JwtMiddleware
 }
 
 func NewUserHandler(e *echo.Echo, u models.UserUsecase) {
-	uh := &UserHandler{usecase: u}
+	jwt := jwtmiddleware.NewJwtMiddlware()
+	uh := &UserHandler{usecase: u, JwtMiddleware: *jwt}
 
 	e.GET("/users", uh.GetUsers)
 	e.GET("/user/:id", uh.GetUser)
+	e.POST("/user", uh.CreateUser)
+	e.PUT("/user", uh.ValidateToken(uh.UpdateUser))
+	e.DELETE("/user", uh.ValidateToken(uh.DeleteUser))
+	e.GET("user/jwt", uh.GetJWT)
 }
 
 func (u *UserHandler) GetUsers(e echo.Context) error {
@@ -57,5 +64,93 @@ func (u *UserHandler) GetUser(e echo.Context) error {
 	return e.JSON(200, models.Response{
 		Message: "Success",
 		Content: user,
+	})
+}
+
+func (u *UserHandler) CreateUser(e echo.Context) error {
+	var user models.User
+
+	if err := e.Bind(&user); err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	err := u.usecase.CreateUser(user)
+	if err != nil && (errors.Is(err, models.ErrEmptyFields) || errors.Is(err, models.ErrAlreadyExists)) {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: models.ErrAlreadyExists.Error() + "or" + models.ErrEmptyFields.Error(),
+		})
+	} else {
+		return e.JSON(200, models.Response{
+			Message: "Success",
+			Content: "user created",
+		})
+	}
+}
+
+func (u *UserHandler) UpdateUser(e echo.Context) error {
+	var user models.User
+
+	if err := e.Bind(&user); err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	err := u.usecase.UpdateUser(user)
+	if err != nil && (errors.Is(err, models.ErrEmptyFields) || errors.Is(err, models.ErrNotFound)) {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: models.ErrNotFound.Error() + "or" + models.ErrEmptyFields.Error(),
+		})
+	} else {
+		return e.JSON(200, models.Response{
+			Message: "Success",
+			Content: "user updated",
+		})
+	}
+}
+
+func (u *UserHandler) DeleteUser(e echo.Context) error {
+	var user models.User
+
+	if err := e.Bind(&user); err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	err := u.usecase.DeleteUser(user.ID)
+	if err != nil && (errors.Is(err, models.ErrNotFound)) {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: models.ErrNotFound.Error(),
+		})
+	} else {
+		return e.JSON(200, models.Response{
+			Message: "Success",
+			Content: "user deleted",
+		})
+	}
+}
+
+func (u *UserHandler) GetJWT(e echo.Context) error {
+	var user models.User
+
+	if err := e.Bind(&user); err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	return e.JSON(200, models.Response{
+		Message: "Success",
+		Content: u.GenerateToken(user),
 	})
 }
