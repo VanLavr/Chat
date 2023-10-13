f27de5282338042f753c025e4f8b33e64e66c03c
VanLavr <lavr.i-n@mail.ru>
2023-10-13 21:57:41 +0300

    chatroom delivery seems to be ready

--- internal/chatroom/delivery/chatroom_handler.go
+++ internal/chatroom/delivery/chatroom_handler.go
@@ -21,6 +21,9 @@ func NewChatroomHandler(e *echo.Echo, u models.ChatroomUsecase) {
 
 	e.POST("/user/enterChatroom", ch.ValidateToken(ch.EnterChatroom))
 	e.GET("/user/:uid/leaveRoom/:chatroom_id", ch.ValidateToken(ch.LeaveChatroom))
+	e.POST("/chatroom", ch.ValidateToken(ch.CreateChat))
+	e.PUT("/chatroom", ch.ValidateToken(ch.UpdateChat))
+	e.DELETE("/chatroom", ch.ValidateToken(ch.DeleteChat))
 }
 
 func (c *ChatroomHandler) EnterChatroom(e echo.Context) error {
@@ -98,5 +101,85 @@ func (c *ChatroomHandler) LeaveChatroom(e echo.Context) error {
 	return e.JSON(200, models.Response{
 		Message: "Success",
 		Content: "user has leaved in chatroom",
+	})
+}
+
+func (c *ChatroomHandler) CreateChat(e echo.Context) error {
+	var chat models.Chatroom
+
+	err := e.Bind(&chat)
+	if err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	if err = c.usecase.CreateChat(chat); err != nil {
+		if errors.Is(err, models.ErrEmptyFields) || errors.Is(err, models.ErrAlreadyExists) {
+			return e.JSON(400, models.Response{
+				Message: "Failure",
+				Content: models.ErrEmptyFields.Error() + " or " + models.ErrAlreadyExists.Error(),
+			})
+		}
+	}
+
+	return e.JSON(200, models.Response{
+		Message: "Success",
+		Content: "Chatroom created",
+	})
+}
+
+func (c *ChatroomHandler) UpdateChat(e echo.Context) error {
+	var chat models.Chatroom
+
+	err := e.Bind(&chat)
+	if err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	if err = c.usecase.UpdateChat(chat); err != nil {
+		if errors.Is(err, models.ErrEmptyFields) || errors.Is(err, models.ErrNotFound) {
+			return e.JSON(400, models.Response{
+				Message: "Failure",
+				Content: models.ErrEmptyFields.Error() + " or " + models.ErrNotFound.Error(),
+			})
+		}
+	}
+
+	return e.JSON(200, models.Response{
+		Message: "Success",
+		Content: "Chatroom updated",
+	})
+}
+
+func (c *ChatroomHandler) DeleteChat(e echo.Context) error {
+	var deletion struct {
+		uid int
+		cid int
+	}
+
+	err := e.Bind(&deletion)
+	if err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: "Invalid params",
+		})
+	}
+
+	err = c.usecase.DeleteChat(deletion.uid, deletion.cid)
+	if err != nil {
+		return e.JSON(400, models.Response{
+			Message: "Failure",
+			Content: models.ErrNotFound.Error() + " or " + models.ErrPermisionDenied.Error(),
+		})
+	}
+
+	return e.JSON(200, models.Response{
+		Message: "Success",
+		Content: "Chatroom deleted",
 	})
 }
